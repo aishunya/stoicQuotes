42656191e1170a0209133361ebffb0e5ff56eee6
Aisha Nurgaliyeva <ayshanurgali@gmail.com>
2023-05-24 11:43:21 +0200

    Initial Commit

---
+++ myAppAishunya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myAppAishunya
+//
+//  Created by Aisha Nurgaliyeva on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
